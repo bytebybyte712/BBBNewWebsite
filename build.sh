c4f0a85a0d84ef00f6c3302d656b0bbab4ce8374
bytebybyte712 <bytebybyte712@gmail.com>
2026-08-03 13:38:06 -0400

    Add Riad Hasan episode three

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 rm -rf dist
 mkdir -p dist/client/assets dist/server
 
-cp index.html about.html podcast.html publications.html peter-aiken.html Romain-Lheritier.html styles.css script.js dist/client/
+cp index.html about.html podcast.html publications.html peter-aiken.html Romain-Lheritier.html riad-hasan.html styles.css script.js dist/client/
 cp -R assets/. dist/client/assets/
 cp worker.js dist/server/index.js
