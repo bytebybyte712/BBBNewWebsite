8c2b1ad96c7b1a3a206f1f0a169e1f74f16b6b12
bytebybyte712 <bytebybyte712@gmail.com>
2026-08-07 13:15:55 -0400

    Remove host name and private About page

--- build.sh
+++ build.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 rm -rf dist
 mkdir -p dist/client/assets dist/server
 
-cp index.html about.html podcast.html publications.html peter-aiken.html Romain-Lheritier.html riad-hasan.html featured-guests.html press-media.html recognition-awards.html speaking.html impact.html press-releases.html styles.css script.js dist/client/
+cp index.html podcast.html publications.html peter-aiken.html Romain-Lheritier.html riad-hasan.html featured-guests.html press-media.html recognition-awards.html speaking.html impact.html press-releases.html styles.css script.js dist/client/
 cp -R assets/. dist/client/assets/
+rm -f dist/client/assets/Vihaan_Publication.jpg \
+  dist/client/assets/vihaan-tsa-awards.jpeg \
+  dist/client/assets/vihaan-state-conference-awards.jpeg \
+  dist/client/assets/vihaan-technosphere-2024.jpeg \
+  dist/client/assets/vihaan-tsa-speaking.jpeg
 cp worker.js dist/server/index.js
